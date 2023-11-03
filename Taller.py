7a37194b5afad132057ba2441e972b1d54492b08
Sandra Junco <smjuncop@gmail.com>
2023-11-03 18:00:00 -0500

    commit_01

---
+++ Taller.py
@@ -0,0 +1,124 @@
+
+grupo_estudiantes = [
+    
+    {
+    'nombre': 'alejandro',
+    'apellido': 'garcía',
+    'promedio': 3.78,
+    'nivel_programación': 2,
+    'edad': 21,
+    'estado_civil': 'soltera',
+    },
+
+    {
+    'nombre': 'juan',
+    'apellido': 'rivera',
+    'promedio': 3.4,
+    'nivel_programación': 3,
+    'edad': 21,
+    'estado_civil': 'soltera',
+    },
+
+    {
+    'nombre': 'brayan',
+    'apellido': 'ortiz',
+    'promedio': 3.09,
+    'nivel_programación': 2,
+    'edad': 20,
+    'estado_civil': 'soltera',
+    },
+
+    {
+    'nombre': 'jhon',
+    'apellido': 'neva',
+    'promedio': 3.5,
+    'nivel_programación': 2,
+    'edad': 20,
+    'estado_civil': 'soltera',
+    },
+
+    {
+    'nombre': 'luisa',
+    'apellido': 'bentacourt',
+    'promedio': 3.5,
+    'nivel_programación': 3,
+    'edad': 19,
+    'estado_civil': 'soltera',
+    },
+
+    {
+    'nombre': 'linda',
+    'apellido': 'ramos',
+    'promedio': 3.61,
+    'nivel_programación': 2,
+    'edad': 25,
+    'estado_civil': 'con novio',
+    },
+
+    {
+    'nombre': 'maria',
+    'apellido': 'pardo',
+    'promedio': 3.8,
+    'nivel_programación': 2,
+    'edad': 23,
+    'estado_civil': 'con novio',
+    },
+
+    {
+    'nombre': 'diana',
+    'apellido': 'soto',
+    'promedio': 3.6,
+    'nivel_programación': 2,
+    'edad': 20,
+    'estado_civil': 'con novio',
+    },
+
+    {
+    'nombre': 'johan',
+    'apellido': 'galindo',
+    'promedio': 3.38,
+    'nivel_programación': 2,
+    'edad': 22,
+    'estado_civil': 'soltera',
+    },
+
+    {
+    'nombre': 'angel',
+    'apellido': 'molano',
+    'promedio': 3.6,
+    'nivel_programación': 3,
+    'edad': 24,
+    'estado_civil': 'con novio',
+    },
+
+    {
+    'nombre': 'nikole',
+    'apellido': 'florez',
+    'promedio': 4.19,
+    'nivel_programación': 2.5,
+    'edad': 21,
+    'estado_civil': 'soltera',
+    },
+
+    {
+    'nombre': 'andres',
+    'apellido': 'segura',
+    'promedio': 4.1,
+    'nivel_programación': 3,
+    'edad': 24,
+    'estado_civil': 'con novio',
+    },
+
+    {
+    'nombre': 'ruth',
+    'apellido': 'duran',
+    'promedio': 4.0,
+    'nivel_programación': 3,
+    'edad': 22,
+    'estado_civil': 'con novio',
+    }
+]
+
+#promedio de edad del curso
+for x in grupo_estudiantes:
+    print()
