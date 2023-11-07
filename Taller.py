2f988465445b3739c1d32a249091c90add40fca8
SandraJunco <142632588+SandraJunco@users.noreply.github.com>
2023-11-07 16:16:09 -0500

    Taller.py

--- Taller.py
+++ Taller.py
@@ -2,120 +2,129 @@
 grupo_estudiantes = [
     
     {
-    'nombre': 'alejandro',
-    'apellido': 'garcía',
+    'nombre': 'Alejandro',
+    'apellido': 'García',
     'promedio': 3.78,
     'nivel_programación': 2,
     'edad': 21,
-    'estado_civil': 'soltera',
+    'estado_civil': 'Soltera',
     },
 
     {
-    'nombre': 'juan',
-    'apellido': 'rivera',
+    'nombre': 'Juan',
+    'apellido': 'Rivera',
     'promedio': 3.4,
     'nivel_programación': 3,
     'edad': 21,
-    'estado_civil': 'soltera',
+    'estado_civil': 'Soltera',
     },
 
     {
-    'nombre': 'brayan',
-    'apellido': 'ortiz',
+    'nombre': 'Brayan',
+    'apellido': 'Ortiz',
     'promedio': 3.09,
     'nivel_programación': 2,
     'edad': 20,
-    'estado_civil': 'soltera',
+    'estado_civil': 'Soltera',
     },
 
     {
-    'nombre': 'jhon',
-    'apellido': 'neva',
+    'nombre': 'Jhon',
+    'apellido': 'Neva',
     'promedio': 3.5,
     'nivel_programación': 2,
     'edad': 20,
-    'estado_civil': 'soltera',
+    'estado_civil': 'Soltera',
     },
 
     {
-    'nombre': 'luisa',
-    'apellido': 'bentacourt',
+    'nombre': 'Luisa',
+    'apellido': 'Bentacourt',
     'promedio': 3.5,
     'nivel_programación': 3,
     'edad': 19,
-    'estado_civil': 'soltera',
+    'estado_civil': 'Soltera',
     },
 
     {
-    'nombre': 'linda',
-    'apellido': 'ramos',
+    'nombre': 'Linda',
+    'apellido': 'Ramos',
     'promedio': 3.61,
     'nivel_programación': 2,
     'edad': 25,
-    'estado_civil': 'con novio',
+    'estado_civil': 'En relación',
     },
 
     {
-    'nombre': 'maria',
-    'apellido': 'pardo',
+    'nombre': 'Maria',
+    'apellido': 'Pardo',
     'promedio': 3.8,
     'nivel_programación': 2,
     'edad': 23,
-    'estado_civil': 'con novio',
+    'estado_civil': 'En relación',
     },
 
     {
-    'nombre': 'diana',
-    'apellido': 'soto',
+    'nombre': 'Diana',
+    'apellido': 'Soto',
     'promedio': 3.6,
     'nivel_programación': 2,
     'edad': 20,
-    'estado_civil': 'con novio',
+    'estado_civil': 'En relación',
     },
 
     {
-    'nombre': 'johan',
-    'apellido': 'galindo',
+    'nombre': 'Johan',
+    'apellido': 'Galindo',
     'promedio': 3.38,
     'nivel_programación': 2,
     'edad': 22,
-    'estado_civil': 'soltera',
+    'estado_civil': 'Soltera',
     },
 
     {
-    'nombre': 'angel',
-    'apellido': 'molano',
+    'nombre': 'Angel',
+    'apellido': 'Molano',
     'promedio': 3.6,
     'nivel_programación': 3,
     'edad': 24,
-    'estado_civil': 'con novio',
+    'estado_civil': 'En relación',
     },
 
     {
-    'nombre': 'nikole',
-    'apellido': 'florez',
+    'nombre': 'Nikole',
+    'apellido': 'Florez',
     'promedio': 4.19,
     'nivel_programación': 2.5,
     'edad': 21,
-    'estado_civil': 'soltera',
+    'estado_civil': 'Soltera',
     },
 
     {
-    'nombre': 'andres',
-    'apellido': 'segura',
+    'nombre': 'Andres',
+    'apellido': 'Segura',
     'promedio': 4.1,
     'nivel_programación': 3,
     'edad': 24,
-    'estado_civil': 'con novio',
+    'estado_civil': 'En relación',
     },
 
     {
-    'nombre': 'ruth',
-    'apellido': 'duran',
+    'nombre': 'Ruth',
+    'apellido': 'Duran',
     'promedio': 4.0,
     'nivel_programación': 3,
     'edad': 22,
-    'estado_civil': 'con novio',
+    'estado_civil': 'En relación',
+    },
+        
+    {
+    'nombre': 'Sandra',
+    'apellido': 'Junco',
+    'promedio': 4.4,
+    'nivel_programación': 2,
+    'edad': 23,
+    'estado_civil': 'Soltera',
     }
 ]
 
